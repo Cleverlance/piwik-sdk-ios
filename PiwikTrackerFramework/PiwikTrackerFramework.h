b5bff7fccf023f2178725f86acdb93291439db9d
pivotal <wedrive-core@googlegroups.com>
2016-05-10 16:08:42 +0200

    make carthage-able

---
+++ PiwikTrackerFramework/PiwikTrackerFramework.h
@@ -0,0 +1,22 @@
+//
+//  PiwikTrackerFramework.h
+//  PiwikTrackerFramework
+//
+//  Created by pivotal on 5/10/16.
+//  Copyright © 2016 Mattias Levin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+
+//! Project version number for PiwikTrackerFramework.
+FOUNDATION_EXPORT double PiwikTrackerFrameworkVersionNumber;
+
+//! Project version string for PiwikTrackerFramework.
+FOUNDATION_EXPORT const unsigned char PiwikTrackerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PiwikTrackerFramework/PublicHeader.h>
+
+
